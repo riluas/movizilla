1fd87b03dfc8fb364ff90123437545a44d8a7b8e
riluas <cutsay48@gmail.com>
2022-04-26 20:22:29 +0200

    hanged side menu routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Home', url: '/folder/Home', icon: 'home' },
+    { title: 'Followed', url: '/folder/Followed', icon: 'heart' },
+    { title: 'Profile', url: '/folder/Profile', icon: 'person-circle' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
 }
