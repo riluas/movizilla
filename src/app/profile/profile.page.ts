26d6bd453eb94700e85632e3f637fd9737ceeb37
riluas <cutsay48@gmail.com>
2022-05-01 13:44:21 +0200

    Designed profile page

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
+  userImage: string;
+  currentUser: string;
+  userSurname: string;
+  email: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.userImage = "https://c.tenor.com/lTtlX5xlfmgAAAAC/nyan-cat.gif";
+    this.currentUser  ="Alfonso";
+    this.userSurname = "Lamar Asis";
+    this.email = "prueba@prueba.es";
   }
 
 }
