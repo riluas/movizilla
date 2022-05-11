387ed678412a4b41a65ab561cfd1a0fee051e749
riluas <cutsay48@gmail.com>
2022-05-12 00:16:00 +0200

    Followed shows movies and redirects to movieDetail

--- src/app/followed/followed.page.ts
+++ src/app/followed/followed.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../services/data.service';
+import {InfiniteScrollCustomEvent, LoadingController} from '@ionic/angular';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-followed',
@@ -7,9 +10,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FollowedPage implements OnInit {
 
-  constructor() { }
+  movies = [];
+  currentPage = 1;
+  imageBaseUrl = environment.images;
 
-  ngOnInit() {
+  constructor(private dataService: DataService,private loadingController: LoadingController) {
+
+    this.dataService.getNotes().subscribe(res =>{
+     // console.log(res);
+    })
+   }
+
+   ngOnInit(){
+    this.loadMovies();
+  }
+
+  async loadMovies(event?: InfiniteScrollCustomEvent){
+   const loading = await this.loadingController.create({
+     message: 'Loading..',
+     spinner: 'bubbles',
+   })
+   await loading.present();
+
+   this.dataService.getTopRatedMovies(this.currentPage).subscribe(res =>{
+     loading.dismiss();
+     this.movies.push(...res.results);
+     console.log(res);
+
+     event?.target.complete();
+     if(event){
+       event.target.disabled = res.total_pages === this.currentPage;
+     };
+   })
+  }
+
+  loadMore(event: InfiniteScrollCustomEvent){
+   this.currentPage++;
+   this.loadMovies(event);
   }
 
 }
